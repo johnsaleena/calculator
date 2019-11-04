a1cf63ac7131770ee30141520d31a1502566356c
johnsaleena <55074280+johnsaleena@users.noreply.github.com>
2019-11-03 21:40:48 -0500

    Update CalculatorTests.py

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,5 +1,7 @@
 import unittest
 from Calculator import Calculator
+from CsvReader import CsvReader
+from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
@@ -10,28 +12,35 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_property_calculator(self):
-        self.assertEqual(self.calculator.result, 0)
-
     def test_add_method_calculator(self):
-        self.assertEqual(self.calculator.add(2, 2), 4)
-        self.assertEqual(self.calculator.result, 4)
+        test_data = CsvReader('src/addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
-    def test_subtract_method_calculator(self):
-        self.assertEqual(self.calculator.subtract(6, 2), 4)
-        self.assertEqual(self.calculator.result, 4)
+    def test_subtract_method_calculator(self): 
+        test_data = CsvReader('src/subtraction.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.subtract(int(row['Value 2']), int(row['Value 1'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_multiply_method_calculator(self):
-        self.assertEqual(self.calculator.multiply(6, 2), 12)
-        self.assertEqual(self.calculator.result, 12)
+        test_data = CsvReader('src/multiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_divide_method_calculator(self):
-        self.assertEqual(self.calculator.divide(6, 2), 3)
-        self.assertEqual(self.calculator.result, 3)
+        test_data = CsvReader('src/division.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.divide(4, 2), 2)
+            self.assertEqual(self.calculator.result, 2)
 
     def test_square_method_calculator(self):
-        self.assertEqual(self.calculator.square(3), 9)
-        self.assertEqual(self.calculator.result, 9)
+        test_data = CsvReader('src/square.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.square(int(row['Value 1'])), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_square_rt_method_calculator(self):
         self.assertEqual(self.calculator.square_rt(16), 4)
